6f855173cda1a8b84ad97c05e71771c1892803ba
Andrew Mcveigh <me@andrewmcveigh.com>
2018-07-15 22:26:51 +0100

    WIP

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,36 +1,21 @@
 module Eval where
 
+import Core
 import qualified Data.Map.Strict as Map
-import Data.Map.Strict (Map)
-import Debug.Trace
-import Control.Monad.Except
-import Control.Monad.Reader
-import Control.Monad.State.Lazy
-import Prelude hiding (lookup, read)
-import Reader (read)
-import Syntax hiding (bind, subst)
+-- import Debug.Trace
+-- import Reader (read)
+import Types
 
 data EvalError
   = ResolveFailure Name
   | CannotApply Expr Expr
-  deriving Show
 
-newtype Env = Env (Map Name Expr)
 
 emptyEnv :: Env
 emptyEnv = Env Map.empty
 
-emptyTypeEnv :: TypeEnv
-emptyTypeEnv = TypeEnv Map.empty
-
-sub :: Name -> Expr -> Expr -> Expr
-sub _ (Fix (Lit x  )) _          = Fix $ Lit x
-sub s (Fix (Sym x  )) e | s == x = e
-sub _ (Fix (Sym x  )) _          = Fix $ Sym x
-sub s (Fix (App a b)) e          = Fix $ App (sub s a e) (sub s b e)
-sub s (Fix (Lam x a)) _ | s == x = Fix $ Lam x a
-sub s (Fix (Lam x a)) e          = Fix $ Lam x (sub s a e)
-sub s (Fix (Tup a b)) e          = Fix $ Tup (sub s a e) (sub s b e)
+-- emptyTypeEnv :: TypeEnv
+-- emptyTypeEnv = TypeEnv Map.empty
 
 type Eval a = ReaderT Env (StateT Env (Except EvalError)) a
 
@@ -39,9 +24,9 @@ lookup sym = do
   (Env lexical) <- ask
   (Env global)  <- get
   case Map.lookup sym lexical of
-    Just e -> return e
+    Just e -> pure e
     Nothing -> case Map.lookup sym global of
-      Just e -> return e
+      Just e -> pure e
       Nothing -> throwError $ ResolveFailure sym
 
 bind :: Name -> Expr -> Eval Expr -> Eval Expr
@@ -49,48 +34,57 @@ bind sym e m = do
   let scope (Env e') = Env $ Map.insert sym e e' in
     local scope m
 
-expr :: Ast -> Expr
-expr (Fix (Node _ (Lit e  ) _)) = Fix $ Lit e
-expr (Fix (Node _ (Sym e  ) _)) = Fix $ Sym e
-expr (Fix (Node _ (Lam x e) _)) = Fix $ Lam x $ expr e
-expr (Fix (Node _ (App a b) _)) = Fix $ App (expr a) (expr b)
-expr (Fix (Node _ (Tup a b) _)) = Fix $ Tup (expr a) (expr b)
-
 apply :: Expr -> Expr -> Eval Expr
-apply (Fix (Lam x e)) b = return $ sub x e b
+apply (Lam x e) b = bind x b (eval e)
 apply e b = throwError $ CannotApply e b
 
-eval :: Ast -> Eval Expr
-eval (Fix (Node _ (Lit a  ) _)) = return $ Fix $ Lit a
-eval (Fix (Node _ (Sym a  ) _)) = lookup a
-eval (Fix (Node _ (Lam x e) _)) = return $ Fix $ Lam x $ trace (show $ expr e) $ expr e
-eval (Fix (Node _ (App a b) _)) = do
+close :: Lambda -> Eval Expr
+close f = do
+  Env lexical <- ask
+  Env global  <- get
+  pure (Clo (Env (Map.union lexical global)) f)
+
+thunk :: Expr -> Eval Expr
+thunk e = do
+  Env lexical <- ask
+  Env global  <- get
+  let f = (Lambda (Name "_") e)
+  pure $ Thk $ Thunk $ Closure (Env (Map.union lexical global)) f
+
+eval :: Expr -> Eval Expr
+eval a@(Lit _) = pure a
+eval (Sym a  ) = lookup a
+eval (Lam x e) = close (Lambda x e)
+eval (App a b) = do
   a' <- eval a
-  b' <- eval b
-  apply a' b'
-eval (Fix (Node _ (Tup a b) _)) = do
-  a' <- eval a
-  b' <- eval b
-  return $ Fix $ Tup a' b'
+  case a' of
+    Prm _ x e -> Eval.apply (Lam x e) b
+    _         -> eval b >>= Eval.apply a'
+eval (Thk t  ) = pure (unThunk t)
+  where
+    unThunk (Thunk (Closure e f)) = Clo e f
+eval (Clo e f) = pure (Clo e f)
+eval _
+  = panic "Impossible pattern match"
 
-data REPLError
-  = TE TypeError
-  | EE EvalError
-  deriving Show
+-- data REPLError
+--   = TE TypeError
+--   | EE EvalError
+--   deriving Show
 
-runTC :: Expr -> Either TypeError Ast
-runTC e =
-  fmap fst $ runExcept (runStateT (runReaderT (infer e) emptyTypeEnv) 0)
+-- runTC :: Expr -> Either TypeError Ast
+-- runTC e =
+--   fmap fst $ runExcept (runStateT (runReaderT (infer e) emptyTypeEnv) 0)
 
-runREPL :: String -> Either REPLError Expr
-runREPL s =
-  let e = read s in
-    case runTC e of
-      Left err -> Left $ TE err
-      Right e' ->
-        case runExcept (runStateT (runReaderT (eval e') emptyEnv) emptyEnv) of
-          Left err -> Left $ EE err
-          Right (e'', _) -> Right e''
+-- runREPL :: String -> Either REPLError Expr
+-- runREPL s =
+--   let e = read s in
+--     case runTC e of
+--       Left err -> Left $ TE err
+--       Right e' ->
+--         case runExcept (runStateT (runReaderT (eval e') emptyEnv) emptyEnv) of
+--           Left err -> Left $ EE err
+--           Right (e'', _) -> Right e''
 
 -- case runTC $ read "(a -> a)" of Right (Fix (Node _ (Lam _ _) _)) -> True
 
--- src/Reader.hs
+++ src/Reader.hs
@@ -1,27 +1,25 @@
 module Reader where
 
-import Data.Char
-import qualified Data.Map.Strict as Map
-import Control.Monad
-import Control.Applicative
-import qualified Prelude as Prelude
-import Prelude hiding (read, until)
-import Syntax hiding (bind)
+import Core
 
-newtype Parser a = Parser { parse :: String -> [(a, String)] }
+import Data.Char (isDigit)
+-- import qualified Data.Map.Strict as Map
+import qualified Data.Text as Text
+import Types
 
-runParser :: Parser a -> String -> a
+newtype Parser a = Parser { parse :: Text -> [(a, Text)] }
+
+runParser :: Parser a -> Text -> a
 runParser m s =
   case parse m s of
-    [(res, [])] -> res
-    [(_,   rs)] -> error "Parser did not consume entire stream."
-    _           -> error "Parser error."
+    [(res, null -> True)] -> res
+    [(_,   _)]            -> panic "Parser did not consume entire stream."
+    _                     -> panic "Parser error."
 
 item :: Parser Char
-item = Parser $ \s ->
-  case s of
-   []     -> []
-   (c:cs) -> [(c, cs)]
+item = Parser $ \case
+  (null -> True) -> []
+  text           -> [(Text.head text, Text.tail text)]
 
 bind :: Parser a -> (a -> Parser b) -> Parser b
 bind p f = Parser $ \s -> concatMap (\(a, s') -> parse (f a) s') $ parse p s
@@ -52,7 +50,7 @@ combine :: Parser a -> Parser a -> Parser a
 combine p q = Parser (\s -> parse p s ++ parse q s)
 
 failure :: Parser a
-failure = Parser (\cs -> [])
+failure = Parser (\_ -> [])
 
 option :: Parser a -> Parser a -> Parser a
 option  p q = Parser $ \s ->
@@ -78,7 +76,7 @@ satisfy :: (Char -> Bool) -> Parser Char
 satisfy p = item `bind` \c ->
   if p c
   then unit c
-  else (Parser (\cs -> []))
+  else (Parser (\_ -> []))
 
 is :: [Char] -> Parser [Char]
 is [] = return []
@@ -90,36 +88,42 @@ oneOf s = satisfy (flip elem s)
 noneOf :: [Char] -> Parser Char
 noneOf s = satisfy (not . (flip elem s))
 
+whitespaceChars :: [Char]
 whitespaceChars = " \n\r\t"
+
+macroChars :: [Char]
 macroChars = ",\";@^`~()[]{}\\"
 
-whitespace :: Parser String
+whitespace :: Parser [Char]
 whitespace = many $ oneOf whitespaceChars
 
-macroTerminating :: Parser String
+macroTerminating :: Parser [Char]
 macroTerminating = some $ oneOf macroChars
 
-tokenEnd :: Parser String
+tokenEnd :: Parser [Char]
 tokenEnd = whitespace <|> macroTerminating
 
-token :: Parser [Char]
-token = many $ noneOf (whitespaceChars ++ macroChars)
+token :: Parser Text
+token = pack <$> (many $ noneOf (whitespaceChars ++ macroChars))
 
-until :: Char -> Parser String
-until c = do s <- many $ noneOf [c]; some $ oneOf [c]; return s
+until :: Char -> Parser Text
+until c = pack <$> (many $ noneOf [c] <* (some $ oneOf [c]))
 
 string :: Parser Expr
-string = do is "\""; e <- until '"'; return $ Fix $ Lit $ Syntax.String e
+string = is "\"" *> (Types.string <$> until '"')
 
 symbol :: Parser Expr
-symbol = do tok <- token; return $ Fix $ Sym $ Name tok
+symbol = Types.symbol <$> token
 
 natural :: Parser Expr
-natural = do s <- some (satisfy isDigit)
-             return $ Fix $ Lit $ Syntax.Integer $ Prelude.read s
+natural = do
+  txt <- some $ satisfy isDigit
+  case Core.read $ pack txt of
+    Just i -> pure $ integer i
+    Nothing -> failure
 
 delimited :: Char -> Parser Expr
-delimited c = do s <- expr; _ <- oneOf [c]; return s
+delimited c = expr <* oneOf [c]
 
 openParen :: Parser Char
 openParen = oneOf "("
@@ -139,102 +143,92 @@ closeBracket = oneOf "]"
 bracketed :: Parser a -> Parser a
 bracketed p = do _ <- openBracket; e <- p; _ <- closeBracket; return e
 
-lambdaInner :: Parser Expr
-lambdaInner = do
-  x <- token
-  whitespace
-  is "->"
-  whitespace
-  e <- expr
-  return $ Fix $ Lam (Name x) e
+application :: Parser Expr
+application = parenthesized $ apply <$> (expr <* whitespace) <*> expr
 
-lambda :: Parser Expr
-lambda = parenthesized lambdaInner
-
-application = parenthesized $ do
-  e1 <- expr
-  whitespace
-  e2 <- expr
-  return $ Fix $ App e1 e2
-
-tuple = parenthesized $ do
-  e1 <- expr
-  whitespace
-  is ","
-  whitespace
-  e2 <- expr
-  return $ Fix $ Tup e1 e2
+-- tuple = parenthesized $ do
+--   e1 <- expr
+--   whitespace
+--   is ","
+--   whitespace
+--   e2 <- expr
+--   return $ Fix $ Tup e1 e2
 
 expr :: Parser Expr
-expr = lambda <|> application <|> tuple <|> string <|> natural <|> symbol
+expr =
+      application
+  -- <|> tuple
+  <|> Reader.string
+  <|> natural
+  <|> Reader.symbol
 
-def :: Parser Def
-def = do
-  openParen
-  name <- token
-  whitespace
-  e <- lambdaInner <|> expr
-  closeParen
-  return $ Def (Name name) e
+-- def :: Parser Def
+-- def = do
+--   openParen
+--   name <- token
+--   whitespace
+--   e <- lambdaInner <|> expr
+--   closeParen
+--   return $ Def (Name name) e
 
-toplevel :: Parser Def
-toplevel = def
+-- toplevel :: Parser Def
+-- toplevel = def
 
-read :: String -> Expr
+read :: Text -> Expr
 read s = runParser expr s
 
-tcon :: Parser Type
-tcon = do
-  c <- oneOf ['A'..'Z']
-  cs <- token
-  return $ Con $ Name $ c : cs
+-- tcon :: Parser Type
+-- tcon = do
+--   c <- oneOf ['A'..'Z']
+--   cs <- token
+--   return $ Con $ Name $ c : cs
 
-tvar :: Parser Type
-tvar = do
-  c <- oneOf ['a'..'z']
-  cs <- token
-  return $ Var $ Name $ c : cs
+-- tvar :: Parser Type
+-- tvar = do
+--   c <- oneOf ['a'..'z']
+--   cs <- token
+--   return $ Var $ Name $ c : cs
 
-arrowInner :: Parser Type
-arrowInner =
-  do a <- type'; whitespace; is "->"; whitespace; b <- type'; return $ Arr a b
+-- arrowInner :: Parser Type
+-- arrowInner =
+--   do a <- type'; whitespace; is "->"; whitespace; b <- type'; return $ Arr a b
 
-arrow :: Parser Type
-arrow = parenthesized arrowInner
+-- arrow :: Parser Type
+-- arrow = parenthesized arrowInner
 
-tapp :: Parser Type
-tapp = parenthesized $ do
-  t1 <- type'
-  t2 <- type'
-  return $ Tap t1 t2
+-- tapp :: Parser Type
+-- tapp = parenthesized $ do
+--   t1 <- type'
+--   t2 <- type'
+--   return $ Tap t1 t2
 
-type' :: Parser Type
-type' = arrow <|> tapp <|> tvar <|> tcon
+-- type' :: Parser Type
+-- type' = arrow <|> tapp <|> tvar <|> tcon
 
-readSig :: String -> Type
-readSig s = runParser type' s
+-- readSig :: String -> Type
+-- readSig s = runParser type' s
 
-subPart :: Parser [(Name, Type)]
-subPart = do
-  (Var a) <- tvar
-  whitespace
-  is "/"
-  whitespace
-  t <- type'
-  return $ [(a, t)]
+-- subPart :: Parser [(Name, Type)]
+-- subPart = do
+--   (Var a) <- tvar
+--   whitespace
+--   is "/"
+--   whitespace
+--   t <- type'
+--   return $ [(a, t)]
 
-moreParts :: Parser [(Name, Type)]
-moreParts = parts <|> unit []
-  where parts = do whitespace; is ","; whitespace
-                   s1 <- subPart
-                   ss <- moreParts
-                   return $ s1 ++ ss
+-- moreParts :: Parser [(Name, Type)]
+-- moreParts = parts <|> unit []
+--   where parts = do whitespace; is ","; whitespace
+--                    s1 <- subPart
+--                    ss <- moreParts
+--                    return $ s1 ++ ss
 
-substitution :: Parser Subst
-substitution = bracketed $ do
-  s1 <- subPart <|> unit []
-  ss <- moreParts
-  return $ Map.fromList $ s1 ++ ss
+-- substitution :: Parser Subst
+-- substitution = bracketed $ do
+--   s1 <- subPart <|> unit []
+--   ss <- moreParts
+--   return $ Map.fromList $ s1 ++ ss
 
-readSub :: String -> Subst
-readSub s = runParser substitution s
+-- readSub :: String -> Subst
+-- readSub s = runParser substitution s
